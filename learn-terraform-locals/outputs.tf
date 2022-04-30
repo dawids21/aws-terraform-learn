cb5cdcfd5a3ea101806574d0e5030c4ce3897535
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-04-30 17:14:49 +0200

    simplify terrafrom config with locals

---
+++ learn-terraform-locals/outputs.tf
@@ -0,0 +1,9 @@
+output "public_dns_name" {
+  description = "Public DNS names of the load balancer for this project"
+  value       = module.elb_http.this_elb_dns_name
+}
+
+output "tags" {
+  description = "Tags"
+  value = local.tags
+}
