477256945a53afd7187badbfd7d339661d510042
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-05-03 21:47:23 +0200

    migrate state to terraform cloud

---
+++ learn-state-migration/main.tf
@@ -0,0 +1,31 @@
+## Terraform configuration
+
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 1.1.0"
+  cloud {
+    organization = "dawids21"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+}
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "3"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
+}
